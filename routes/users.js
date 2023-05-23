c6d6f9694d6be17c8976f40995402ad77cee7a57
CEPHAL0 <xaradxarma@gmail.com>
2023-05-23 10:51:03 +0545

    Error Handling Middleware:

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,21 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
+// Error handling middleware
+const errorHandler = (err, req, res, next) => {
+    if (err instanceof multer.MulterError) {
+        // Handle multer errors
+        if (err.code === 'LIMIT_FILE_SIZE') {
+            return res.status(400).json({ message: 'File Size Limit Exceeded' });
+        }
+    } else if (err) {
+        // Handle other errors
+        return res.status(400).json({ message: err.message });
+    }
+
+    next(err);
+}
+
 // Multer for file uploads
 const storage = multer.diskStorage({
     destination: (req, res, cb) => {
@@ -19,8 +34,11 @@ const storage = multer.diskStorage({
         if (fileExtension == '.jpg' || fileExtension == '.jpeg' || fileExtension == '.gif' || fileExtension == '.png') {
             uniqueFileName = `${Date.now()}${fileExtension}`;
             req.uniqueFileName = uniqueFileName;
+            cb(null, uniqueFileName);
+        } else {
+            cb(new Error('Invalid file extension'), null);
         }
-        cb(null, uniqueFileName);
+
     }
 });
 
@@ -28,6 +46,7 @@ const upload = multer({ storage });
 
 
 // Register a new user
+// upload.single for single file uploads
 router.post('/register', upload.single('profilePicture'), async (req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -47,6 +66,10 @@ router.post('/register', upload.single('profilePicture'), async (req, res) => {
 
         const user = new User({ username, email, password });
 
+        if (req.fileValidationError) {
+            // Handle the file validation error
+            return res.status(400).json({ message: req.fileValidationError });
+        }
         // Update the profile picture if it exists
         if (req.file) {
             const uniqueFileName = req.uniqueFileName;
@@ -55,10 +78,13 @@ router.post('/register', upload.single('profilePicture'), async (req, res) => {
         await user.save();
         res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
-        res.status(500).json({ message: "Failed to register user", error: error.message })
+        res.status(500).json({ message: "Failed to register user", error: error.message });
+        next(error);
     }
 });
 
+// Register the error handling middleware
+router.use(errorHandler);
 
 // Authenticate user and generate JWT
 router.post('/login', async (req, res) => {
