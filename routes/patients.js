40fb1441b68497bed21ca380a05747bee26f6733
CEPHAL0 <xaradxarma@gmail.com>
2023-05-24 10:27:45 +0545

    Complete CRUD operation for Patient and Parsing of Form using Multer

--- routes/patients.js
+++ routes/patients.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const Patient = require("../models/patient");
+const multer = require("multer");
 
-router.post('/register', async (req, res) => {
+
+// Making an instance of multer to properly encode the input form data
+const upload = multer();
+
+// Create new Patients
+router.post('/register', upload.none(), async (req, res) => {
     try {
         const { name, age, gender, contactNumber, address, numberOfAppointments } = req.body;
-
-        if (!numberOfAppointments) {
-            numberOfAppointments = 0;
-        }
 
         const patient = await Patient.create({ name, age, gender, contactNumber, address, numberOfAppointments });
 
@@ -25,13 +27,79 @@ router.post('/register', async (req, res) => {
     }
 });
 
+
+// Get all Patients Details
 router.get('/', async (req, res) => {
     try {
         const patients = await Patient.find();
-        res.status(200).json(patients);
+        res.status(200).json({ patients: patients });
     } catch (error) {
         res.status(500).json({ message: 'Failed to retrieve patients', error })
     }
 });
+
+// Get Patient by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const patient = await Patient.findById(id);
+
+        if (!patient) {
+            return res.status(404).json({ message: "Patient Not Found" });
+        }
+
+        res.status(200).json(patient)
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to Retrieve Patient', error: error.message })
+    }
+})
+
+
+// Update Patient
+// Using multer middleware to parse form data
+router.put('/update/:id', upload.none(), async (req, res) => {
+    try {
+        const { name, age, address, numberOfAppointments, contactNumber, gender } = req.body;
+
+        const { id } = req.params;
+        const patient = await Patient.findById(id);
+
+        if (!patient) {
+            return res.status(404).json({ message: "Patient Not Found" })
+        }
+
+        patient.name = name;
+        patient.age = age;
+        patient.address = address;
+        patient.numberOfAppointments = numberOfAppointments;
+        patient.contactNumber = contactNumber;
+        patient.gender = gender;
+
+        await patient.save();
+
+        return res.status(200).json({ message: "Patient Updated Successfully", updatedPatient: patient });
+
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to Update Patient Details", error: error.message })
+    }
+})
+
+
+// Deleting user by id
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const patient = await Patient.findById(id);
+
+        if (!patient) {
+            return res.status(404).json({ message: "Patient Not Found" });
+        }
+
+        await patient.deleteOne();
+        return res.status(200).json({ message: "Patient Record Deleted Successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: "Cannot Delete Patient Details", error: error.message })
+    }
+})
 
 module.exports = router;
